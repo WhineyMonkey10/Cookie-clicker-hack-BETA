1feac85bb00140c1b779b8584da336d3ec017aff
GBJ <gabriel@bialy.ca>
2022-11-18 19:18:28 +0100

    Big updates, fixed a few things and added a console.

--- main.js
+++ main.js
@@ -4,13 +4,22 @@
 // Enjoy!
 
 // User Interface, real one coming soon
-main();
+wait(1000);
+alert("🍪 Welcome to Cookie Clicker Hack! 🍪\n\nPress CTRL + ALT + C to open the cheat panel. \n\n🍪 Enjoy! 🍪");
+
+// Run the main function
+
 
 // Main function
 
+document.addEventListener("keyup",function(evt){
+    if(evt.keyCode=="67"&&evt.altKey&&evt.ctrlKey){
+        main();
+    }});
+
 function main() {
     // Get the user's input
-    var input = prompt("Add cookies (1)       set cookies (2)       ruin the fun (3)       set bakery name (4)       get all upgrades (5)       get all achievements (6)       more coming soon <3");
+    var input = prompt("                                           🍪 -- Cheat Panel -- 🍪\n\nAdd Cookies (1) \n\n Set Cookies (2) \n\n Ruin the Fun (3) \n\n Set Bakery Name (4) \n\n Get All Upgrades (Doesn't Work) (5) \n\n Get All Achievements (Half Works) (6) \n\n🍪 More Coming Soon <3 🍪");
     // Check if the user's input is valid
     if (input == null || input == "") {
         // If the user's input is invalid, show an error
@@ -23,30 +32,36 @@ function main() {
             let cookies = prompt("How many cookies would you like to add?");
             
             Game.cookies = Game.cookies + parseInt(cookies);
+            exit();
         } else if (input == "2") {
             let cookies = prompt("How many cookies would you like to set?");
             
             Game.cookies = parseInt(cookies);
+            exit();
         } else if (input == "3") {
             Game.RuinTheFun();
-
+            exit();
         } else if (input == "4") {
             let name = prompt("What would you like to name your bakery?");
             Game.bakeryName=name;
             Game.bakeryNameRefresh();
+            exit();
         } else if (input == "5") {
-            alert("This does not currently work.")
-            Game.UpgradesById.forEach(function(upgrade) {
-                upgrade.bought = 1;
-            });
+            Game.SetAllUpgrade(1);
+            exit();
         } else if (input == "6") {
-            alert("This does not currently work.")
-            Game.AchievementsById.forEach(function(achievement) {
-                achievement.won = 1;
-            });
+            Game.SetAllAchievs(1);
+            exit();
         } else {
             alert("Invalid input!");
         }
     }
 }
 
+function wait(ms) {
+    var start = new Date().getTime();
+    var end = start;
+    while (end < start + ms) {
+        end = new Date().getTime();
+    }
+}
