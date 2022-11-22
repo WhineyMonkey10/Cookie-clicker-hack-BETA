c39a6d499bd17a118ed8c1ee516a76748fc4c6f0
GBJ <gbialystokjoly@gmail.com>
2022-11-22 07:42:07 +0100

    Added python :)

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ document.addEventListener("keyup",function(evt){
 
 function main() {
     // Get the user's input
-    var input = prompt("                                           🍪 -- Cheat Panel -- 🍪\n\nAdd Cookies (1) \n\n Set Cookies (2) \n\n Ruin the Fun (3) \n\n Set Bakery Name (4) \n\n Get All Upgrades (Doesn't Work) (5) \n\n Get All Achievements (Half Works) (6)\n\n Dump Logs (7) \n\n🍪 More Coming Soon <3 🍪");
+    var input = prompt("                                           🍪 -- Cheat Panel -- 🍪\n\nAdd Cookies (1) \n\n Set Cookies (2) \n\n Ruin the Fun (3) \n\n Set Bakery Name (4) \n\n Get All Upgrades (Doesn't Work) (5) \n\n Get All Achievements (Half Works) (6)\n\n Dump Logs (7) \n\n Set CPS (Don't Click the Cookie once used) (11)\n\n🍪 More Coming Soon <3 🍪");
     // Check if the user's input is valid
     if (input == null || input == "") {
         // If the user's input is invalid, show an error
